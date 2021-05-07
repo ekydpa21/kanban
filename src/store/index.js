4bf5525e71e30812cbbf94c2c213ece463091093
ekydpa21 <ekydodipratamaa21@gmail.com>
2021-05-07 23:52:30 +0700

    Membereskan styling

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import todosReducer from "./reducers/todosReducer";
-import taskReducer from "./reducers/taskReducer";
 
 const rootReducer = combineReducers({
   todos: todosReducer,
-  tasks: taskReducer,
 });
 
 let store = createStore(rootReducer, applyMiddleware(thunk));
